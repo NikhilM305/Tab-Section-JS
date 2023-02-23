ba460d4738399b428d65fdccceabb4691917adde
NikhilM305 <nikhil.ninja007@gmail.com>
2023-02-23 23:46:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const tabs=document.querySelector(".tabs")
+const btns=document.querySelectorAll(".button")
+const articles=document.querySelectorAll(".content")
+
+tabs.addEventListener("click",(e)=>{
+console.log(e.target.dataset.nik);
+    const id=e.target.dataset.nik ;
+    console.log(id)
+  if(id){
+    btns.forEach((btn)=>{
+        btn.classList.remove("live");
+    })
+    e.target.classList.add("live");
+articles.forEach((article)=>{
+  article.classList.remove("live")
+})
+
+    const element=document.getElementById(id);
+    element.classList.add("live");
+  }
+})
